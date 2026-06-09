e7f22e32ca7f99ffb507948c334b0b7dba243fbc
alcaram07 <53909553+alcaram07@users.noreply.github.com>
2026-06-09 11:57:56 -0300

    fix: re-generate migrations to solve PendingModelChangesWarning in production

---
+++ server/Migrations/20260609145756_FixedBarberosFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedBarberosFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
